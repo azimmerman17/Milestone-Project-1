deea0b62039e505adba2ed600c380c2e71f1f926
Andrew Zimmerman <azimmerman@pga.com>
2022-11-03 20:30:43 -0400

    Removed placeholder cards from html and build function to intiate game and assign a dealer

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const suits = ['spades', 'diamonds', 'clubs', 'hearts'];
 const faceValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let ai = {score: 0, dealer: false, playPoints: 0, handPoints: 0, cribPoints: 0}
+let player = {score: 0, dealer: false, playPoints: 0, handPoints: 0, cribPoints: 0}
 
 //builds an unshuffled deck for the game !!USE LISTENER TO SHUFFLE DECK WHEN USER CLICKS START GAME!!
 const deck = getDeck()
@@ -15,30 +17,55 @@ function getDeck() {
 			 	case 'A':
 					card.gameValue = 1
 			 		card.rank = 1
+                    card.hex = '1'
 			 		break;
+                case '10':
+                    card.gameValue = 10
+			 		card.rank = 10
+                    card.hex = 'A'
+                    break
 			 	case 'J':
 					card.gameValue = 10
 			 		card.rank = 11
+                    card.hex = 'B'
 			 		break;
 				case 'Q':
 					card.gameValue = 10
 					card.rank = 12
+                    card.hex = 'B'
 					break;
 				case 'K':
 					card.gameValue = 10
 					card.rank = 13
+                    card.hex = 'B'
 					break;
 				default:
 					card.gameValue = card.faceValue/1
 					card.rank = card.gameValue/1
+                    card.hex = card.faceValue
 					break;		
 			};
-		};
+            //unicode for the card 'text' content.  Unicodes found here: https://www.htmlsymbols.xyz/games-symbols/playing-cards
+            switch (card.suit) {
+                case 'spades':
+                    card.hexcode = '&#x1F0A' + card.hex
+                    break
+                case 'diamonds':
+                    card.hexcode = '&#x1F0C' + card.hex
+                    break
+                case 'clubs':
+                    card.hexcode = '&#x1F0D' + card.hex
+                    break
+                case 'hearts':
+                    card.hexcode = '&#x1F0B' + card.hex
+            
+            }
+        }
 	};
 	return deck;
 };
 
-//shuffles the deck when needed -always need to calls a deck var
+//shuffles the deck when needed -always need to calls a deck varible
 function shuffle(deck) {
 	for (let i = 0; i < 1000; i++) {
 		let card1 = Math.floor(Math.random() * 52)
@@ -51,9 +78,6 @@ function shuffle(deck) {
 	return deck
 };
 
-//shuffle(deck)
-console.log(deck)
-playDeck = cutDeck(deck)
 //function to cut the deck
 function cutDeck(playDeck) {
     let cut = Math.floor(Math.random() * 52) //Bonus Time: Listener to allow user to pick card
@@ -63,19 +87,59 @@ function cutDeck(playDeck) {
     let newDeck = deckCut.concat(tempDeck)
     return newDeck
 }
-console.log(playDeck)
 
 //cut cards to see who deals first
-	//p1 cuts
-	//p2 cuts
-		//low card deals
+//p1 cuts  //p2 cuts  //low card deals --same card redo
+function getDealer() {
+        console.log(deck)
+        let cut = Math.floor(Math.random() * 52) 
+        console.log(cut)
+        while (cut <10 || cut > 48) {
+            cut = Math.floor(Math.random() * 52)
+        }
+        console.log(cut)
+        let playerCard = deck[cut]
+        let aiCard = deck[Math.floor(Math.random() * cut)]
+        console.log(playerCard)
+        console.log(aiCard)
+
+        if (playerCard.rank < aiCard.rank) {
+        player.dealer = true
+        } else if (playerCard.rank > aiCard.rank) {
+            ai.dealer = true
+        } else {
+            shuffle(deck)
+            getDealer()
+        }   
+}
+
+startGame()
+
+async function startGame() {
+    shuffle(deck)
+    const dealer = document.getElementById('deck')
+    while (player.dealer === false && ai.dealer === false) {
+        dealer.addEventListener('click', (() => {
+            getDealer()
+            console.log(player)
+            console.log(ai)
+        }))
+    }
+    // while (player.score <= 120 || ai.score <= 120) {
+    await shuffle(deck)
+    console.log(player)
+    console.log(ai)
+
+        
+    // }
+}
 
 //start game -while loop to END GAME
 
 	//start round
 		//shuffle deck
-		//cut deck - non dealer
-		// deal 6 cards -- non dealer first
+
+        		// deal 6 cards -- non dealer first
 			//either .pop() or for loop - put in in a new arrey
 
 		//players discard 2 to crib
